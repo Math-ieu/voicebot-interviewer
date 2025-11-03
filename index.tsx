84f2fed7389edc90e66256b04b5dbf5971953e40
Math-ieu <atsoumathieu228@gmail.com>
2025-11-04 00:01:12 +0100

    feat: integrate Vercel Analytics into the application

--- index.tsx
+++ index.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Analytics } from "@vercel/analytics/next"
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -11,6 +12,7 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Analytics/>
+     <App />
+  </React.StrictMode> 
 );
